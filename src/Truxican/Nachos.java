5326783a54ec018bde7a3a3e425bf58bedd3a2c8
RichardF12 <00168117@uca.edu.sv>
2018-06-05 19:14:48 -0600

    Creacion de la base para los nachos y su interfaz

---
+++ src/Truxican/Nachos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Truxican;
+
+/**
+ *
+ * @author ricky
+ */
+public class Nachos implements Abilities {
+    private int price;
+    private int life;
+    private final int ATKUnit = 20;
+    private final int ATKEdif = 10; 
+
+    public Nachos(int price, int life) {
+        this.price = price;
+        this.life = life;
+    }
+        
+    @Override
+    public void Attack() {
+
+        if(Target == Units[]){//Hay que crear un array que contenga todas las unidades de todas las razas y otro igual para edificaciones.
+            System.out.println("Tck, Tck, Tck");
+            while(Target.life != 0){
+                Target.life == Target.life - ATKUnit;
+            }
+            System.out.println("Ba Dum Tzz");
+            Target.delete();
+        }else{//cuando estan atacando una edificacion
+            System.out.println("Destrucciooooooon weeeeeeeee!!!");
+            while(Target.life != 0){
+                Target.life == Target.life - ATKEdif;
+            }
+            System.out.println("Yeiiiiiiii!!!");
+            Target.delete();
+        }
+            
+    }
+
+    @Override
+    public void Defend() {
+        
+    }
+    
+}
