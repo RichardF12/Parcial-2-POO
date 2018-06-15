36c248d40033771643110b6a810adfdca042099b
RichardF12 <00168117@uca.edu.sv>
2018-06-14 20:28:09 -0600

    haciendo el menu mas visible

--- src/Main/Menu2.java
+++ src/Main/Menu2.java
@@ -14,6 +14,7 @@ public class Menu2 {
     public static void MenuInicio(){
         Scanner scan = new Scanner(System.in);
         
+        System.out.println("-----------------------------------------");
         System.out.print("Jugador 1 Ingrese su usuario: ");
         String user1 = scan.next();
         System.out.println("");
@@ -23,18 +24,18 @@ public class Menu2 {
         System.out.println("2)Eagles");
         System.out.println("3)Kamikazes");
         System.out.print("Escoja su raza: ");
-        int race1 = scan.nextInt();
-        switch(race1){
+        int race1 = scan.nextInt(); 
+            switch(race1){
             case 1:
                 //Import Truxican(?)
             case 2:
                 //Import Eagles(?)
             case 3:
                 //Import Kamikazes(?)
-        }
-        System.out.println("");
+            }
+        System.out.println("----------------------------------------");
         System.out.print("Jugador 2 Ingrese su usuario: ");
-        String user2 = scan.next();
+        String user = scan.next();
         System.out.println("");
         System.out.println("RAZAS");
         System.out.println("-----");
